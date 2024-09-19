112e491e7dd4603d017fb81d78b611d6b36ae7b0
basha-github <sharifabee.alur@gmail.com>
2024-09-19 11:17:12 +0530

    updated for above 10 rupees

--- IfElseDemo.java
+++ IfElseDemo.java
@@ -7,8 +7,26 @@ public class IfElseDemo {
 		//Write a program to count total number of notes in given amount
 		
 		// n--8== 5 2 1
-		int money = 8,c5=0,c2=0,c1=0;
+		int money = 0,c2000=0,c500=0,c200=0,c100=0,c50=0,
+				c20=0,c10=0,c5=0,c2=0,c1=0;
 		String totalVal = "";
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("enter money");
+		money = sc.nextInt();
+		
+		if(money > 20) {
+			c10= money /20;
+			totalVal = totalVal + "20 notes--->"+c20+"\n";
+			money = money - (c20*20);
+		}
+		
+		if(money > 10) {
+			c10= money /10;
+			totalVal = totalVal + "10 notes--->"+c10+"\n";
+			money = money - (c10*10);
+		}
 		if(money > 5) {
 			c5= money /5;
 			totalVal = totalVal + "5 coins--->"+c5+"\n";
